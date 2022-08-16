06af6397f24e0a95acf3967b1ab672ada305fb53
marwa <mds08@mail.aub.edu>
2022-08-16 23:27:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries= batteryBatches.reduce((previousValue, currentValue) => parseFloat(previousValue) + parseFloat(currentValue), 0);
+
+// const totalBatteries = batteryBatches.reduce((acc, ele) => {
+//     return acc + ele;
+//  })
